c550fe1b931f0456055766b4dd3dfd85dcf602d1
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-08-25 09:59:37 +0200

    replace bytes with chars in test/text.lua

--- plugin/resurrect/test/text.lua
+++ plugin/resurrect/test/text.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local pub = {}
 
-function pub.write_all_bytes(pane)
+function pub.write_all_chars(pane)
 	-- ascii
 	for i = 1, 127 do
 		pane:inject_output(string.char(i))
@@ -16,11 +16,11 @@ function pub.write_and_save_current_window()
 	return wezterm.action_callback(function(win, pane)
 		local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 		local mux_win = win:mux_window()
-		pub.write_all_bytes(pane)
-		mux_win:set_title("TEST_WRITING_BYTES")
+		pub.write_all_chars(pane)
+		mux_win:set_title("TEST_WRITING_CHARS")
 		local state = resurrect.window_state.get_window_state(mux_win)
 		resurrect.save_state(state)
-		wezterm.log_info("SAVED THE WINDOW AS TEST_WRITING_BYTES")
+		wezterm.log_info("SAVED THE WINDOW AS TEST_WRITING_CHARS")
 	end)
 end
 
